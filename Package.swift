b8659bc569c769bf9360c3ff08436f27bb180e9a
David Battefeld <davidbattefeld@gmail.com>
2024-12-29 14:53:29 +0100

    fix: add a product to package

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SkylarksAPIClient",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
+    products: [
+        .library(name: "SkylarksAPIClient", targets: ["SkylarksAPIClient"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
         .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
